e7099142724db39740bc3489f93a8fe7df3fe80f
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2018-07-04 15:40:38 -0300

    Refactoring and created RectTransformNode.

---
+++ source/Assets/Style/Editor/RectTransformNodeEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using XNodeEditor;
+
+[CustomNodeEditor(typeof(RectTransformNode))]
+public class RectTransformNodeEditor : NodeEditor
+{
+    
+    public override void OnBodyGUI()
+    {
+        NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("origin"));
+
+        SerializedProperty xOrLeftProp = serializedObject.FindProperty("xOrLeft");
+        SerializedProperty yOrTopProp = serializedObject.FindProperty("yOrTop");
+        SerializedProperty widthOrRightProp = serializedObject.FindProperty("widthOrRight");
+        SerializedProperty heightOrBottomProp = serializedObject.FindProperty("heightOrBottom");
+        SerializedProperty anchorMinProp = serializedObject.FindProperty("anchorMin");
+        SerializedProperty anchorMaxProp = serializedObject.FindProperty("anchorMax");
+        SerializedProperty pivotProp = serializedObject.FindProperty("pivot");
+        
+        NodeEditorGUILayout.PropertyField(xOrLeftProp, new GUIContent(anchorMinProp.vector2Value.x == anchorMaxProp.vector2Value.x ? "Pos X" : "Left"));
+        NodeEditorGUILayout.PropertyField(yOrTopProp, new GUIContent(anchorMinProp.vector2Value.y == anchorMaxProp.vector2Value.y ? "Pos Y" : "Top"));
+        NodeEditorGUILayout.PropertyField(widthOrRightProp, new GUIContent(anchorMinProp.vector2Value.x == anchorMaxProp.vector2Value.x ? "Width" : "Right"));
+        NodeEditorGUILayout.PropertyField(heightOrBottomProp, new GUIContent(anchorMinProp.vector2Value.y == anchorMaxProp.vector2Value.y ? "Height" : "Bottom"));
+        NodeEditorGUILayout.PropertyField(anchorMinProp);
+        NodeEditorGUILayout.PropertyField(anchorMaxProp);
+        NodeEditorGUILayout.PropertyField(pivotProp, new GUIContent("Pivot (raw)"));
+    }
+}
---
+++ source/Assets/Style/Nodes/RectTransformNode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateNodeMenu("Style/Component/Rect Transform")]
+public class RectTransformNode : StyleNode
+{
+
+    [Input] public float xOrLeft = 0;
+    [Input] public float widthOrRight = 0;
+    [Input] public float yOrTop = 100;
+    [Input] public float heightOrBottom = 100;
+    [Input] public Vector2 anchorMin = new Vector2(0.5f, 0.5f);
+    [Input] public Vector2 anchorMax = new Vector2(0.5f, 0.5f);
+    [Input] public Vector2 pivot = new Vector2(0.5f, 0.5f);
+
+    public override void Apply(GameObject obj)
+    {
+        base.Apply(obj);
+
+        if (obj == null) return;
+
+        RectTransform r = obj.GetComponent<RectTransform>();
+
+        if (r == null) return;
+
+        r.anchorMin = anchorMin;
+        r.anchorMax = anchorMax;
+        r.pivot = pivot;
+        
+        if (anchorMin.x == anchorMax.x)
+        {
+            r.anchoredPosition = new Vector2(xOrLeft, r.anchoredPosition.y);
+            r.sizeDelta = new Vector2(widthOrRight, r.sizeDelta.y);
+        }
+        else
+        {
+            r.offsetMin = new Vector2(xOrLeft, r.offsetMin.y);
+            r.offsetMax = new Vector2(- widthOrRight, r.offsetMax.y);
+        }
+
+        if (anchorMin.y == anchorMax.y)
+        {
+            r.anchoredPosition = new Vector2(r.anchoredPosition.x, yOrTop);
+            r.sizeDelta = new Vector2(r.sizeDelta.x, heightOrBottom);
+        }
+        else
+        {
+            r.offsetMax = new Vector2(r.offsetMax.x, -yOrTop);
+            r.offsetMin = new Vector2(r.offsetMin.x, heightOrBottom);
+        }
+    }
+}
